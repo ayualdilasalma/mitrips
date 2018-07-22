3eb79d2156e613884f8214c13a0825dac4f6e0a2
ayu aldila salma <ayu.aldilas@yahoo.com>
2018-07-22 19:36:25 +0800

    add footer

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+const Footer = styled.footer`
+  text-align: center;
+`;
+
+export default Footer;
---
+++ app/components/Footer/index.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * Footer
+ *
+ */
+
+import React from 'react';
+import Wrapper from './Footer';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <Wrapper>
+      <p>Footer</p>
+    </Wrapper>
+  );
+}
+
+Footer.propTypes = {};
+
+export default Footer;
